83845026ca7f7a9ed96c688b8b93fec6735db9b5
juliethiebaut <askjuliethiebaut@gmail.com>
2022-04-22 00:33:09 +0200

    3 files modified

--- script.js
+++ script.js
@@ -194,14 +194,14 @@ const config = {
           color: 'none',
         },
         pointLabels: {
-          fontSize: 16,
+          font: {
+            size: 16,
+          },
         },
       },
     },
   },
 };
-
-Chart.defaults.font.size = 16;
 
 const myChart = new Chart(document.getElementById('myChart'), config);
 
@@ -209,10 +209,22 @@ Chart.defaults.plugins.legend.position = 'top';
 
 const stackedBarData = {
   labels: [
-    'Trouvez-vous que le réchauffement climatique soit un sujet sérieusement pris en compte dans votre département/service/unité de travail ?',
-    'Vous donne-t-on les moyens de mesurer ou de faire diminuer les émissions de gaz à effet de serre des produits ou services sur lesquels vous travaillez ?',
-    'Pensez-vous que votre travail contribue à faire baisser les émissions de gaz à effet de serre de la société dans son ensemble ?',
-    'Savez-vous si votre entreprise adapte votre lieu de travail et votre travail aux conséquences à venir du changement climatique ?',
+    [
+      'Trouvez-vous que le réchauffement climatique soit un sujet ',
+      'sérieusement pris en compte dans votre département/service/unité de travail ?',
+    ],
+    [
+      'Vous donne-t-on les moyens de mesurer ou de faire diminuer les émissions ',
+      'de gaz à effet de serre des produits ou services sur lesquels vous travaillez ?',
+    ],
+    [
+      'Pensez-vous que votre travail contribue à faire baisser',
+      'les émissions de gaz à effet de serre de la société dans son ensemble ?',
+    ],
+    [
+      'Savez-vous si votre entreprise adapte votre lieu de travail et ',
+      'votre travail aux conséquences à venir du changement climatique ?',
+    ],
   ],
   datasets: [
     {
@@ -279,9 +291,18 @@ const myChart2 = new Chart(document.getElementById('myChart2'), stackedBar);
 
 const stackedBarData3 = {
   labels: [
-    "Trouvez-vous que l'effondrement de la biodiversité soit un sujet sérieusement pris en compte dans votre département/service/unité de travail ?",
-    "Vous donne-t-on les moyens de mesurer ou de faire diminuer l'impact sur la biodiversité des produits ou service sur lesquels vous travaillez ?",
-    'Pensez-vous que votre travail contribue à faire baisser la destruction de la biodiversité de la société dans son ensemble ?',
+    [
+      "Trouvez-vous que l'effondrement de la biodiversité soit un sujet ",
+      'sérieusement pris en compte dans votre département/service/unité de travail ?',
+    ],
+    [
+      "Vous donne-t-on les moyens de mesurer ou de faire diminuer l'impact ",
+      'sur la biodiversité des produits ou service sur lesquels vous travaillez ?',
+    ],
+    [
+      'Pensez-vous que votre travail contribue à faire baisser ',
+      'la destruction de la biodiversité de la société dans son ensemble ?',
+    ],
   ],
   datasets: [
     {
@@ -348,9 +369,18 @@ const myChart3 = new Chart(document.getElementById('myChart3'), stackedBar3);
 
 const stackedBarData4 = {
   labels: [
-    "Trouvez-vous que l'effondrement de la biodiversité soit un sujet sérieusement pris en compte dans votre département/service/unité de travail ?",
-    "Vous donne-t-on les moyens de mesurer ou de faire diminuer l'impact sur la biodiversité des produits ou service sur lesquels vous travaillez ?",
-    'Pensez-vous que votre travail contribue à faire baisser la destruction de la biodiversité de la société dans son ensemble ?',
+    [
+      "Trouvez-vous que l'effondrement de la biodiversité soit un sujet ",
+      'sérieusement pris en compte dans votre département/service/unité de travail ?',
+    ],
+    [
+      "Vous donne-t-on les moyens de mesurer ou de faire diminuer l'impact ",
+      'sur la biodiversité des produits ou service sur lesquels vous travaillez ?',
+    ],
+    [
+      'Pensez-vous que votre travail contribue à faire baisser ',
+      'la destruction de la biodiversité de la société dans son ensemble ?',
+    ],
   ],
   datasets: [
     {
@@ -417,9 +447,18 @@ const myChart4 = new Chart(document.getElementById('myChart4'), stackedBar4);
 
 const stackedBarData5 = {
   labels: [
-    "Trouvez-vous que la réduction de la surconsommation de matériaux et d'eau soit un sujet sérieusement pris en compte dans votre département/service/unité de travail ?",
-    "Vous donne-t-on les moyens de mesurer ou de faire diminuer la consommation de matériaux et d'eau des produits ou service sur lesquels vous travaillez ?",
-    "Pensez-vous que votre travail contribue à faire baisser la consommation de matériaux et d'eau de la société dans son ensemble ?",
+    [
+      "Trouvez-vous que la réduction de la surconsommation de matériaux et d'eau ",
+      'soit un sujet sérieusement pris en compte dans votre département/service/unité de travail ?',
+    ],
+    [
+      'Vous donne-t-on les moyens de mesurer ou de faire diminuer la consommation',
+      " de matériaux et d'eau des produits ou service sur lesquels vous travaillez ?",
+    ],
+    [
+      'Pensez-vous que votre travail contribue à faire baisser ',
+      "la consommation de matériaux et d'eau de la société dans son ensemble ?",
+    ],
   ],
   datasets: [
     {
@@ -486,11 +525,26 @@ const myChart5 = new Chart(document.getElementById('myChart5'), stackedBar5);
 
 const stackedBarData6 = {
   labels: [
-    "Vous a-t-on déjà proposé une formation, ou a minima des activités sérieuses d'information et de sensibilisation, sur les enjeux écologiques ?",
-    "Vous a-t-on déja proposé une formation qui vous apporterait des compétences nécessaires pour participer à la transformation écologique de l'activité de votre entreprise ?",
-    'Votre département/service/unité a-t-il un plan sérieux de réduction des impacts dûs à son fonctionnement quotidien ?',
-    "Lors de votre dernier entretien annuel, les enjeux écologiques de l'entreprise ont-ils fait partie de la discussion ?",
-    "Etes-vous sollicité par votre direction lors de l'élaboration de la stratégie de votre entreprise en matière d'enjeux écologiques ?",
+    [
+      'Vous a-t-on déjà proposé une formation, ou a minima des activités ',
+      "sérieuses d'information et de sensibilisation, sur les enjeux écologiques ?",
+    ],
+    [
+      'Vous a-t-on déja proposé une formation qui vous apporterait des compétences ',
+      "nécessaires pour participer à la transformation écologique de l'activité de votre entreprise ?",
+    ],
+    [
+      'Votre département/service/unité a-t-il un plan sérieux ',
+      'de réduction des impacts dûs à son fonctionnement quotidien ?',
+    ],
+    [
+      'Lors de votre dernier entretien annuel, les enjeux ',
+      "écologiques de l'entreprise ont-ils fait partie de la discussion ?",
+    ],
+    [
+      "Etes-vous sollicité par votre direction lors de l'élaboration ",
+      "de la stratégie de votre entreprise en matière d'enjeux écologiques ?",
+    ],
   ],
   datasets: [
     {
