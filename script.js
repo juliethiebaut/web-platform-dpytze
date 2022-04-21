2b52d027828ba4bb4c53e31498d72b4ab3c00712
juliethiebaut <askjuliethiebaut@gmail.com>
2022-04-21 23:54:42 +0200

    2 files modified

--- script.js
+++ script.js
@@ -343,3 +343,212 @@ const stackedBar3 = {
 };
 
 const myChart3 = new Chart(document.getElementById('myChart3'), stackedBar3);
+
+const stackedBarData4 = {
+  labels: [
+    "Trouvez-vous que l'effondrement de la biodiversité soit un sujet sérieusement pris en compte dans votre département/service/unité de travail ?",
+    "Vous donne-t-on les moyens de mesurer ou de faire diminuer l'impact sur la biodiversité des produits ou service sur lesquels vous travaillez ?",
+    'Pensez-vous que votre travail contribue à faire baisser la destruction de la biodiversité de la société dans son ensemble ?',
+  ],
+  datasets: [
+    {
+      label: 'Oui',
+      data: [40, 40, 67],
+      fill: true,
+      backgroundColor: 'rgba(31, 156, 77, 0.2)',
+      borderColor: 'rgb(31, 156, 77)',
+      pointBackgroundColor: 'rgb(31, 156, 77)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(31, 156, 77)',
+    },
+    {
+      label: 'Non',
+      data: [50, 40, 33],
+      fill: true,
+      backgroundColor: 'rgba(242, 0, 0, 0.2)',
+      borderColor: 'rgb(171, 171, 171)',
+      pointBackgroundColor: 'rgb(242, 0, 0)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(242, 0, 0)',
+    },
+    {
+      label: 'Non pertinent',
+      data: [10, 20, 0],
+      fill: true,
+      backgroundColor: 'rgba(171, 171, 171, 0.2)',
+      borderColor: 'rgb(171, 171, 171)',
+      pointBackgroundColor: 'rgb(171, 171, 171)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(171, 171, 171)',
+    },
+  ],
+};
+
+const stackedBar4 = {
+  type: 'bar',
+  data: stackedBarData,
+  options: {
+    indexAxis: 'y',
+    scales: {
+      x: {
+        stacked: true,
+        min: 0,
+        max: 100,
+      },
+      y: {
+        stacked: true,
+      },
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: "Lutte contre l'effondrement de la biodiversité",
+      },
+    },
+  },
+};
+
+const myChart4 = new Chart(document.getElementById('myChart4'), stackedBar4);
+
+const stackedBarData5 = {
+  labels: [
+    "Trouvez-vous que la réduction de la surconsommation de matériaux et d'eau soit un sujet sérieusement pris en compte dans votre département/service/unité de travail ?",
+    "Vous donne-t-on les moyens de mesurer ou de faire diminuer la consommation de matériaux et d'eau des produits ou service sur lesquels vous travaillez ?",
+    "Pensez-vous que votre travail contribue à faire baisser la consommation de matériaux et d'eau de la société dans son ensemble ?",
+  ],
+  datasets: [
+    {
+      label: 'Oui',
+      data: [40, 40, 67],
+      fill: true,
+      backgroundColor: 'rgba(31, 156, 77, 0.2)',
+      borderColor: 'rgb(31, 156, 77)',
+      pointBackgroundColor: 'rgb(31, 156, 77)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(31, 156, 77)',
+    },
+    {
+      label: 'Non',
+      data: [50, 40, 33],
+      fill: true,
+      backgroundColor: 'rgba(242, 0, 0, 0.2)',
+      borderColor: 'rgb(171, 171, 171)',
+      pointBackgroundColor: 'rgb(242, 0, 0)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(242, 0, 0)',
+    },
+    {
+      label: 'Non pertinent',
+      data: [10, 20, 0],
+      fill: true,
+      backgroundColor: 'rgba(171, 171, 171, 0.2)',
+      borderColor: 'rgb(171, 171, 171)',
+      pointBackgroundColor: 'rgb(171, 171, 171)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(171, 171, 171)',
+    },
+  ],
+};
+
+const stackedBar5 = {
+  type: 'bar',
+  data: stackedBarData,
+  options: {
+    indexAxis: 'y',
+    scales: {
+      x: {
+        stacked: true,
+        min: 0,
+        max: 100,
+      },
+      y: {
+        stacked: true,
+      },
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: 'Réduction de consommation de ressources',
+      },
+    },
+  },
+};
+
+const myChart5 = new Chart(document.getElementById('myChart5'), stackedBar5);
+
+const stackedBarData6 = {
+  labels: [
+    "Vous a-t-on déjà proposé une formation, ou a minima des activités sérieuses d'information et de sensibilisation, sur les enjeux écologiques ?",
+    "Vous a-t-on déja proposé une formation qui vous apporterait des compétences nécessaires pour participer à la transformation écologique de l'activité de votre entreprise ?",
+    'Votre département/service/unité a-t-il un plan sérieux de réduction des impacts dûs à son fonctionnement quotidien ?',
+    "Lors de votre dernier entretien annuel, les enjeux écologiques de l'entreprise ont-ils fait partie de la discussion ?",
+    "Etes-vous sollicité par votre direction lors de l'élaboration de la stratégie de votre entreprise en matière d'enjeux écologiques ?",
+  ],
+  datasets: [
+    {
+      label: 'Oui',
+      data: [40, 40, 67, 50, 20],
+      fill: true,
+      backgroundColor: 'rgba(31, 156, 77, 0.2)',
+      borderColor: 'rgb(31, 156, 77)',
+      pointBackgroundColor: 'rgb(31, 156, 77)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(31, 156, 77)',
+    },
+    {
+      label: 'Non',
+      data: [50, 40, 33, 25, 40],
+      fill: true,
+      backgroundColor: 'rgba(242, 0, 0, 0.2)',
+      borderColor: 'rgb(171, 171, 171)',
+      pointBackgroundColor: 'rgb(242, 0, 0)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(242, 0, 0)',
+    },
+    {
+      label: 'Non pertinent',
+      data: [10, 20, 0, 25, 40],
+      fill: true,
+      backgroundColor: 'rgba(171, 171, 171, 0.2)',
+      borderColor: 'rgb(171, 171, 171)',
+      pointBackgroundColor: 'rgb(171, 171, 171)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(171, 171, 171)',
+    },
+  ],
+};
+
+const stackedBar6 = {
+  type: 'bar',
+  data: stackedBarData,
+  options: {
+    indexAxis: 'y',
+    scales: {
+      x: {
+        stacked: true,
+        min: 0,
+        max: 100,
+      },
+      y: {
+        stacked: true,
+      },
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: 'Conséquences pour les salariés',
+      },
+    },
+  },
+};
+
+const myChart6 = new Chart(document.getElementById('myChart6'), stackedBar6);
